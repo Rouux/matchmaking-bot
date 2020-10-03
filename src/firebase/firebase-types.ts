eed874494720f9d7a8f5912effabf23ced167b3b
unknown <thraverdy@gmail.com>
2020-10-03 14:02:56 +0200

    ffs

---
+++ src/firebase/firebase-types.ts
@@ -0,0 +1,3 @@
+export type CollectionReference = FirebaseFirestore.CollectionReference;
+export type DocumentReference = FirebaseFirestore.DocumentReference;
+export type WriteResult = FirebaseFirestore.WriteResult;
